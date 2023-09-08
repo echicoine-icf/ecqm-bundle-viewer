b46eeb6f53bf301d4dbe3e7cb6cc3ff11f2b36c2
Evan Chicoine <evan.chicoine@esacinc.com>
2023-09-08 13:47:14 -0400

    Reduced complexity of TabManager

--- src/main/java/ui/TabManager.java
+++ src/main/java/ui/TabManager.java
@@ -4,8 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -22,10 +22,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
-import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Document;
@@ -46,72 +43,45 @@ public class TabManager {
 	private static final String RESOURCE_TYPE = "resourceType";
 	private static final String ENTRY = "entry";
 
-	public static void createJsonTab(String fileName, String fileContent) {
-		JSONObject jsonObject = new JSONObject(fileContent);
+    public static void createJsonTab(String fileName, String fileContent) {
+        JSONObject jsonObject = new JSONObject(fileContent);
+        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(BundleViewerMain.tabbedPane);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel searchPanel = new JPanel(new BorderLayout());
+        JPanel searchInputPanel = new JPanel();
+        searchInputPanel.setLayout(new BoxLayout(searchInputPanel, BoxLayout.X_AXIS));
 
-		JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(BundleViewerMain.tabbedPane);
+        JTextField searchTextField = new JTextField();
+        JTextArea textArea = new JTextArea(20, 60);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        JCheckBox multiSearchCheckBox = new JCheckBox("Multi-highlight");
 
-		JPanel mainPanel = new JPanel(new BorderLayout());
+        JLabel searchLabel = new JLabel("Highlight:");
+        searchLabel.setBorder(new EmptyBorder(0, 8, 0, 0));
+        searchLabel.setMaximumSize(searchLabel.getPreferredSize());
 
-		// Create the "Search:" label and text field
-		JPanel searchPanel = new JPanel(new BorderLayout());
+        searchInputPanel.add(searchLabel);
+        searchInputPanel.add(searchTextField);
+        searchInputPanel.add(Box.createHorizontalGlue());
+        searchInputPanel.add(multiSearchCheckBox);
+        searchPanel.add(searchInputPanel, BorderLayout.NORTH);
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
 
-		JPanel searchInputPanel = new JPanel();
-		searchInputPanel.setLayout(new BoxLayout(searchInputPanel, BoxLayout.X_AXIS)); // Use BoxLayout
-
-		JLabel searchLabel = new JLabel("Highlight:");
-		JTextField searchTextField = new JTextField();
-
-		// Create a multiline text field
-		JTextArea textArea = new JTextArea(20, 60);
-		JScrollPane scrollPane = new JScrollPane(textArea);
-
-		// Create the "Multi-search" checkbox
-		JCheckBox multiSearchCheckBox = new JCheckBox("Multi-highlight");
-
-		// Add padding to the label
-		searchLabel.setBorder(new EmptyBorder(0, 8, 0, 0)); // 8 pixels of padding on the left
-
-		// Set the maximum size of the label to its preferred size
-		searchLabel.setMaximumSize(searchLabel.getPreferredSize());
-
-		// Add the components to the search input panel
-		searchInputPanel.add(searchLabel);
-		searchInputPanel.add(searchTextField);
-		searchInputPanel.add(Box.createHorizontalGlue());
-		searchInputPanel.add(multiSearchCheckBox);
-
-		// Add the search input panel to the search panel
-		searchPanel.add(searchInputPanel, BorderLayout.NORTH);
-
-		// Add the search panel and text area to the main panel
-		mainPanel.add(searchPanel, BorderLayout.NORTH); // Place the search components at the top
-		mainPanel.add(scrollPane, BorderLayout.CENTER); // Place the text area to fill the remaining space
-
-		searchTextField.addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				int keyCode = e.getKeyCode();
-				if (KeyEvent.getKeyText(keyCode).equalsIgnoreCase("Enter")) {
-					if (!multiSearchCheckBox.isSelected()) {
+        searchTextField.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                	if (!multiSearchCheckBox.isSelected()) {
 						removeAllHighlights(textArea);
 					}
 					// Highlight text
 					if (searchTextField.getText() != null && searchTextField.getText().length() > 0) {
 						// Checkbox is unchecked
-						invokeHighlight(textArea, searchTextField.getText());
+						highlightText(textArea, searchTextField.getText());
 					}
-				}
-			}
-		});
+                }
+            }
+        });
 
 		multiSearchCheckBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
@@ -119,130 +89,83 @@ public class TabManager {
 					removeAllHighlights(textArea);
 					if (searchTextField.getText() != null && searchTextField.getText().length() > 0) {
 						// Checkbox is unchecked
-						invokeHighlight(textArea, searchTextField.getText());
+						highlightText(textArea, searchTextField.getText());
 					}
 				}
 			}
 		});
 
-		// Check if the top-level object has an "entry" array and "resourceType" is
-		// "Bundle"
-		if (jsonObject.has(ENTRY) && jsonObject.has(RESOURCE_TYPE)
-				&& jsonObject.getString(RESOURCE_TYPE).equals(BUNDLE)) {
-			JSONArray jsonArray = jsonObject.getJSONArray(ENTRY);
-			DefaultListModel<String> resourceListModel = new DefaultListModel<>();
+     // Check if the top-level object has an "entry" array and "resourceType" is "Bundle"
+        if (jsonObject.has(ENTRY) && jsonObject.has(RESOURCE_TYPE) && jsonObject.getString(RESOURCE_TYPE).equals(BUNDLE)) {
+            JSONArray jsonArray = jsonObject.getJSONArray(ENTRY);
+            DefaultListModel<String> resourceListModel = new DefaultListModel<>();
+            resourceListModel.addElement(FULL_JSON_FILE);
 
-			// Add the top-level JSON as the "Full JSON File" entry
-			resourceListModel.addElement(FULL_JSON_FILE);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject entryObject = jsonArray.getJSONObject(i);
+                JSONObject resourceObject = entryObject.optJSONObject(RESOURCE);
+                String title = (resourceObject != null && resourceObject.has(TITLE))
+                        ? resourceObject.getString(TITLE)
+                        : "Resource " + (i + 1);
+                resourceListModel.addElement(title);
+            }
 
-			// Iterate through the "entry" array
-			for (int i = 0; i < jsonArray.length(); i++) {
-				JSONObject entryObject = jsonArray.getJSONObject(i);
-				if (entryObject.has(RESOURCE) && entryObject.getJSONObject(RESOURCE).has(TITLE)) {
-					// Use the "title" field if it exists
-					resourceListModel.addElement(entryObject.getJSONObject(RESOURCE).getString(TITLE));
-				} else {
-					// Use a default title
-					resourceListModel.addElement("Resource " + (i + 1));
-				}
-			}
+            JList<String> resourceList = new JList<>(resourceListModel);
+            resourceList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            resourceList.addListSelectionListener(e -> {
+                int selectedIndex = resourceList.getSelectedIndex();
+                if (selectedIndex == 0) {
+                    textArea.setText(fileContent);
+                } else {
+                    JSONObject selectedResource = jsonArray.getJSONObject(selectedIndex - 1);
+                    textArea.setText(selectedResource.toString(4));
+                }
+                textArea.setCaretPosition(0);
+                searchTextField.setText("");
+            });
 
-			JList<String> resourceList = new JList<>(resourceListModel);
-			resourceList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            if (resourceListModel.getSize() > 0) {
+                resourceList.setSelectedIndex(0);
+            }
 
-			// Add a ListSelectionListener to handle list item clicks
-			resourceList.addListSelectionListener(new ListSelectionListener() {
-				public void valueChanged(ListSelectionEvent event) {
-					if (!event.getValueIsAdjusting()) {
-						int selectedIndex = resourceList.getSelectedIndex();
-						if (selectedIndex == 0) {
-							// Display the full JSON file
-							textArea.setText(fileContent);
-						} else {
-							// Display the JSON snippet for the selected resource
-							if (jsonArray != null) {
-								JSONObject selectedResource = jsonArray.getJSONObject(selectedIndex - 1);
-								String snippet = selectedResource.toString(4); // 4-space indentation
-								textArea.setText(snippet);
-							}
-						}
-						textArea.setCaretPosition(0); // Scroll to the top
-						// reset highlighter
-						searchTextField.setText("");
-					}
-				}
-			});
+            JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(resourceList), mainPanel);
+            splitPane.setResizeWeight(0.25);
+            int listWidth = (int) (splitPane.getWidth() * 0.25);
+            resourceList.setPreferredSize(new Dimension(listWidth, resourceList.getPreferredSize().height));
+            BundleViewerMain.tabbedPane.addTab(fileName, splitPane);
+            BundleViewerMain.tabbedPane.setSelectedIndex(BundleViewerMain.tabbedPane.getTabCount() - 1);
 
-			if (resourceList.getModel().getSize() > 0) {
-				resourceList.setSelectedIndex(0);
-			}
+        } else {
+            JOptionPane.showMessageDialog(frame, Constants.ERROR_INVALID_JSON, Constants.ERROR_TITLE, JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
-			// Create a split pane to display list and text
-			JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(resourceList),
-					mainPanel);
+    public static void highlightText(JTextComponent textComponent, String textToHighlight) {
+    	if (textToHighlight == null || !(textToHighlight.length() >= 2)) {
+    		//bug exists where single characters hang the app up in highlighting. Minimum 2 characters needed.
+    		return;
+    	}
+        try {
+            Highlighter highlighter = textComponent.getHighlighter();
+            Document doc = textComponent.getDocument();
+            String text = doc.getText(0, doc.getLength());
+            int startPos = 0;
 
-			// Set the resize weight to allocate 25% of the width to the list
-			splitPane.setResizeWeight(0.25);
+            while (startPos < text.length()) {
+                int pos = text.indexOf(textToHighlight, startPos);
+                if (pos == -1) {
+                    break;
+                }
+                highlighter.addHighlight(pos, pos + textToHighlight.length(), DefaultHighlighter.DefaultPainter);
+                startPos = pos + textToHighlight.length();
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+    }
 
-			// Calculate the width of the list to be 25% of the total width of the split
-			// pane
-			int totalWidth = splitPane.getWidth();
-			int listWidth = (int) (totalWidth * 0.25);
-
-			// Set the preferred size of the list component
-			resourceList.setPreferredSize(new Dimension(listWidth, resourceList.getPreferredSize().height));
-
-			// Add the new tab with the split pane
-			BundleViewerMain.tabbedPane.addTab(fileName, splitPane);
-
-			// Set focus to the newly created tab
-			BundleViewerMain.tabbedPane.setSelectedIndex(BundleViewerMain.tabbedPane.getTabCount() - 1);
-
-		} else {
-			JOptionPane.showMessageDialog(frame, Constants.ERROR_INVALID_JSON, Constants.ERROR_TITLE,
-					JOptionPane.ERROR_MESSAGE);
-		}
-	}
-
-	public static void invokeHighlight(JTextComponent textComponent, String textToHighlight) {
-		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-			@Override
-			protected Void doInBackground() throws Exception {
-				if (textToHighlight.length() > 1) {
-					highlightText(textComponent, textToHighlight);
-				}
-				return null;
-			}
-		};
-		worker.execute();
-	}
-
-	public static void highlightText(JTextComponent textComponent, String textToHighlight) {
-		try {
-			Highlighter highlighter = textComponent.getHighlighter();
-			Document doc = textComponent.getDocument();
-			String text = doc.getText(0, doc.getLength());
-			int startPos = 0;
-
-			while (startPos < text.length()) {
-				int pos = text.indexOf(textToHighlight, startPos);
-				if (pos == -1) {
-					break; // No more occurrences found
-				}
-
-				// Scroll to the first occurrence of the highlighted text
-				highlighter.addHighlight(pos, pos + textToHighlight.length(), DefaultHighlighter.DefaultPainter);
-
-				// Move the starting position after the occurrence to avoid infinite looping
-				startPos = pos + textToHighlight.length();
-			}
-		} catch (BadLocationException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void removeAllHighlights(JTextComponent textComponent) {
-		Highlighter highlighter = textComponent.getHighlighter();
-		highlighter.removeAllHighlights();
-	}
+    public static void removeAllHighlights(JTextComponent textComponent) {
+        Highlighter highlighter = textComponent.getHighlighter();
+        highlighter.removeAllHighlights();
+    }
 }
